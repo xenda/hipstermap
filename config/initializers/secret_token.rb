0a0358babc1a6acd0d6481a6f0435a54fec27091
Alvaro Pereyra <alvaro@xendacentral.com>
2012-05-16 19:19:13 -0500

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipstermap::Application.config.secret_token = '20d281dc371f10fd6bc200b0025beece9c5b5278b01a571042b0506aea02edf6a86a8c0c2c370a081078de4c65a9d0c152e3313a6189c3a04613e7f89e9c90b0'
