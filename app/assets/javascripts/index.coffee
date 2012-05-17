99073d086426f3efdd398817eac579e5e3b13299
Alvaro Pereyra <alvaro@xendacentral.com>
2012-05-16 20:54:07 -0500

    Updates with Heroku support

--- app/assets/javascripts/index.coffee
+++ app/assets/javascripts/index.coffee
@@ -1,5 +1,7 @@
-(jQuery)->
-  new GMaps
+$(document).ready ->
+
+  map = new GMaps
     div: '#map'
     lat: -12.043333
     lng: -77.028333
+
--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,4 @@
+ENV['REDISTOGO_URL'] = "redis://yaraher:66788abaf8019a891bdee0c2a697a0b8@lab.redistogo.com:9130/"
 REDIS = if ENV['REDISTOGO_URL']
           uri = URI.parse(ENV['REDISTOGO_URL'])
           Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
